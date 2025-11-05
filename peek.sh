54a53e7bdf2a227a5586aa5a8a62641f08d14024
diegovicente <diego.vicente01@estudiant.upf.edu>
2025-11-05 21:57:27 +0100

    third commit

--- peek.sh
+++ peek.sh
@@ -1,3 +1,17 @@
-head -n $2 $1
-echo ...
-tail -n $2 $1
+if [[ -z $2 ]]; then
+	arg2=3   # default value
+else
+	arg2=$2  # use user-provided value
+fi
+
+lines=$(wc -l < $1)
+
+if [[ $lines -le $((2*$arg2)) ]]; then
+	cat $1
+else
+	echo Warning: the number of lines of file $1 exceeds 2 times $arg2
+	echo Here are the first 3 and last 3 lines of file $1
+	head -n $arg2 $1
+	echo ...
+	tail -n $arg2 $1
+fi
